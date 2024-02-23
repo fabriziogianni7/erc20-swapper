9f44a40a6d4f0c441c832448aacffddcdb57f9c4
Fabriziogianni7 <fabrizio@keyko.io>
2024-02-23 10:18:12 +0100

    managing errros + tests

--- contracts/ERC20Swapper.sol
+++ contracts/ERC20Swapper.sol
@@ -1,59 +1,123 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-// Uncomment this line to use console.log
-import "hardhat/console.sol";
-
-import {IUniswapV2Factory} from "node_modules/@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import {IUniswapV2Factory} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 import {IERC20Swapper} from "./interfaces/IERC20Swapper.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IUniswapV2Router02} from "./interfaces/IUniswapV2Router02.sol";
+import {IUniswapV2Pair} from "./interfaces/IUniswapV2Pair.sol";
 
 contract ERC20Swapper is IERC20Swapper {
+
+    error notValidSwapToken(address token);
+    error notEnoughLiquidityInPool();
+    error swapFailed(string reason);
+
+
     IUniswapV2Router02 router =
         IUniswapV2Router02(0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008);
+    
+    bool locked = false;
+
+    /**
+     @notice impeeding potential attackers from calling our swap recursively
+     @notice while the attacker is not incentivated to make a reentrancy attack in this case (he need to send ETH everytime he call swapEtherToToken), we are trusting a 3rd party contract (the router) and we may be subject to changes of state on that contract.
+     */ 
+    modifier avoidReentrancy() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
 
     constructor() {}
 
     ////////////////////////////// SWAP //////////////////////////////
+    /**
+    @notice revert by default is the swapped amount is lower than minAmount
+     */
     function swapEtherToToken(
         address token,
         uint minAmount
-    ) external payable override returns (uint) {
+    ) external payable override avoidReentrancy() returns (uint) {
+        if (!_isTokenSwappable(token)){
+            revert notValidSwapToken(token);
+        }
+        if (!_hasPairLiquidityForSwap(token,minAmount)){
+            revert notEnoughLiquidityInPool();
+        }
+
         address WETH = router.WETH();
 
         address[] memory path = new address[](2);
         path[0] = WETH;
         path[1] = token;
 
-        uint[] memory amounts = router.swapExactETHForTokens{value:msg.value}(
+        try router.swapExactETHForTokens{value: msg.value}(
             minAmount,
             path,
             msg.sender,
-            block.timestamp + 100
-        );
-        return amounts[1];
+            block.timestamp
+        ) returns (uint[] memory amounts) {
+            return amounts[amounts.length - 1];
+        }catch Error(string memory reason) {
+            revert swapFailed(reason);
+        } catch (bytes memory) {
+            revert("Unknown error occurred");
+        }
     }
-    
-    function isTokenSwappable(address token) public view returns (bool) {
-        address factoryAddress = router.factory();
+
+    ////////////////////////////// INTERNAL FUNCTIONS //////////////////////////////
+    function _isTokenSwappable(address token) internal view returns (bool) {
         address WETH = router.WETH();
-        IUniswapV2Factory factory = IUniswapV2Factory(factoryAddress);
+        IUniswapV2Factory factory = IUniswapV2Factory(router.factory());
         if (factory.getPair(WETH, token) == address(0)) {
             return false;
         }
         return true;
     }
 
+    function _hasPairLiquidityForSwap(address token, uint minAmount) internal view returns (bool) {
+        if (!_isTokenSwappable(token)){
+            revert notValidSwapToken(token);
+        }
+        address WETH = router.WETH();
+        IUniswapV2Factory factory = IUniswapV2Factory(router.factory());
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(WETH,token));
+
+        address token0 = pair.token0();
+        (uint112 reserve0, uint112 reserve1,)  = pair.getReserves();
+
+        if (token0 == token){
+            return _checkLiquidity(minAmount,reserve0);
+        }else{
+            return _checkLiquidity(minAmount,reserve1);
+        }
+    }
+
+    function _checkLiquidity(uint minAmount, uint reserve) pure internal returns(bool){
+        if (minAmount > reserve){
+                return false;
+            }
+            return true;
+    }
+
+
+    ////////////////////////////// PUBLIC GETTERS //////////////////////////////
     function getAmountsOut(
         uint amountIn,
         address[] memory path
     ) public view returns (uint[] memory) {
+        if (!_isTokenSwappable(path[1])){
+            uint[] memory zeroOut = new uint[](2);
+            zeroOut[0]=amountIn;
+            zeroOut[1]=0;
+            return zeroOut;
+        }
         return router.getAmountsOut(amountIn, path);
     }
 
-    ////////////////////////////// PUBLIC GETTERS //////////////////////////////
     function getRouterFactory() public view returns (address) {
         return router.factory();
     }
--- test/testSwapper.ts
+++ test/testSwapper.ts
@@ -6,7 +6,7 @@ import * as dotenv from "dotenv";
 import { erc20 } from "../typechain-types/factories/@openzeppelin/contracts/token";
 import { Contract } from "hardhat/internal/hardhat-network/stack-traces/model";
 import * as ERC20CJson from "@openzeppelin/contracts/build/contracts/ERC20.json";
-import { CORRECT_SEPOLIA_WETH, NOT_SWAPPABLE_TOKEN, SEPOLIA_ROUTER_FACTORY_ADDRESS, TOKEN_TO_SWAP } from "./utils/constants";
+import { CORRECT_SEPOLIA_WETH, NOT_SWAPPABLE_TOKEN, SEPOLIA_ROUTER_FACTORY_ADDRESS, TOKEN_TO_SWAP, TOKEN_WITH_NO_LIQUIDITY } from "./utils/constants";
 
 dotenv.config();
 
@@ -16,7 +16,8 @@ describe("Test ERC20Swapper Contract", function () {
   let account_3: Signer
   let swapper: ERC20Swapper
   let swapperAddress: string
-  let swapToken: ERC20 
+  let swapToken: ERC20
+  let WETH: string
   beforeEach(async () => {
     [account_1, account_2, account_3] = await ethers.getSigners();
 
@@ -24,6 +25,7 @@ describe("Test ERC20Swapper Contract", function () {
     swapper = await SwapperFactory.deploy()
     swapperAddress = await swapper.getAddress()
     swapToken = new ethers.Contract(TOKEN_TO_SWAP, ERC20CJson.abi, ethers.provider) as unknown as ERC20
+    WETH = await swapper.getRouterWETH()
   })
   it("testing deployment", async () => {
     expect(await swapToken.getAddress()).equals(TOKEN_TO_SWAP)
@@ -33,33 +35,60 @@ describe("Test ERC20Swapper Contract", function () {
     const routerFactory = await swapper.getRouterFactory()
     expect(routerFactory).equals(SEPOLIA_ROUTER_FACTORY_ADDRESS)
   })
-  it("chack that WETH address is the one on sepolia", async () => {
-    const WETH = await swapper.getRouterWETH()
+  it("check that WETH address is the one on sepolia", async () => {
     expect(WETH).equals(CORRECT_SEPOLIA_WETH)
   })
-  it("test isTokenSwappable", async () => {
-    let isSwappable;
-    isSwappable = await swapper.isTokenSwappable(TOKEN_TO_SWAP)
-    expect(isSwappable).true
-    const shouldNotBeSwappable = NOT_SWAPPABLE_TOKEN
-    isSwappable = await swapper.isTokenSwappable(shouldNotBeSwappable)
-    expect(isSwappable).false
+  it("test getAmountsOut", async () => {
+    const amountToSwap = parseUnits("1", "ether")
+    let amountOut;
+    amountOut = await swapper.getAmountsOut(amountToSwap, [WETH,TOKEN_TO_SWAP])
+    expect(amountOut[1]).greaterThan(0)
+
+    amountOut = await swapper.getAmountsOut(amountToSwap, [WETH,NOT_SWAPPABLE_TOKEN])
+    expect(amountOut[1]).equals(0)
   })
   it("test swap", async () => {
-    const WETH = await swapper.getRouterWETH()
     const initialBalance = await ethers.provider.getBalance(account_1)
     const amountToSwap = parseUnits("1", "ether")
 
     const amountsOut = await swapper.getAmountsOut(amountToSwap, [WETH, TOKEN_TO_SWAP])
+    const minAmount = amountsOut[1]
 
     // SWAP
-    await swapper.connect(account_1).swapEtherToToken(TOKEN_TO_SWAP, amountsOut[1], { value: amountToSwap });
+    await swapper.connect(account_1).swapEtherToToken(TOKEN_TO_SWAP, minAmount, { value: amountToSwap });
 
     const swapTokenBalanceAfterSwap = await swapToken.balanceOf(account_1)
     const ETHbalanceAfterSwap = await ethers.provider.getBalance(account_1)
 
     expect(swapTokenBalanceAfterSwap).greaterThanOrEqual(amountsOut[1])
     expect(ETHbalanceAfterSwap).lessThan(initialBalance - amountsOut[0])
+  })
+  it("test swap - fail case: setting minAmount too high", async () => {
+    const amountToSwap = parseUnits("1", "ether")
+
+    const amountsOut = await swapper.getAmountsOut(amountToSwap, [WETH, TOKEN_TO_SWAP])
+    const minAmount = amountsOut[1] + 1000_000_000_000_000_000n
+
+    // SWAP
+    await expect(swapper.connect(account_1).swapEtherToToken(TOKEN_TO_SWAP, minAmount, { value: amountToSwap })).revertedWithCustomError(swapper, "swapFailed");
+
+  })
+  it("test swap - fail case: the token is not tradable (no pools available)", async () => {
+    const amountToSwap = parseUnits("1", "ether")
+
+    const minAmount = 1000_000_000_000_000_000n
+
+    // SWAP
+    await expect(swapper.connect(account_1).swapEtherToToken(NOT_SWAPPABLE_TOKEN, minAmount, { value: amountToSwap })).revertedWithCustomError(swapper, "notValidSwapToken");
+
+  })
+  it("test swap - fail case: the pool has not enough liquidity for the swap", async () => {
+    const amountToSwap = parseUnits("1", "ether")
+
+    const minAmount = 1000_000_000_000_000_000n
+    // SWAP
+    await expect(swapper.connect(account_1).swapEtherToToken(TOKEN_WITH_NO_LIQUIDITY, minAmount, { value: amountToSwap })).revertedWithCustomError(swapper, "notEnoughLiquidityInPool");
+
   })
 
 });
